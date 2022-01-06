7ae512390ef3f724edf76deb3c5bf5f27c8154a7
GerardoLopez94 <gerardo.a.lopez1994@gmail.com>
2022-01-06 15:51:17 -0300

    imp-exp

---
+++ 02-intro-javascript/src/bases/08-imp-exp.js
@@ -0,0 +1,10 @@
+//importamos los archivos
+import { heroes } from '../data/heroes';
+
+const getHeroeById = (id) => ( heroes.find( (heroe) => heroe.id === id  )  );
+
+const getHeroeByOwner = (owner) => ( heroes.filter( (heroe) => heroe.owner === owner  )  );
+
+console.log(getHeroeById(3));
+
+console.log(getHeroeByOwner('DC'));
