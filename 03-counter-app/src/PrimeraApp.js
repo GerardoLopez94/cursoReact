48a2eebf894c6191e93a5add2ccef9a44ae7d6a6
GerardoLopez94 <gerardo.a.lopez1994@gmail.com>
2022-01-10 13:37:02 -0300

    Fragment

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,7 +1,12 @@
 //FC
 import React from "react";
 const PrimerApp = () => {
-    return <h1>Hola Mundo</h1>
+    return (
+        <>
+            <h1>Hola Mundo</h1>
+            <p>Hola</p>
+        </>
+        );
 }
 
 export { PrimerApp }
