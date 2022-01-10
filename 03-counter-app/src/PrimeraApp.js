33ca71af504b9691acf554e8789c19630897f08e
GerardoLopez94 <gerardo.a.lopez1994@gmail.com>
2022-01-10 13:48:59 -0300

    const en jsx

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,10 +1,21 @@
 //FC
 import React from "react";
 const PrimerApp = () => {
+    const saludo = 'Hola Mundo, Hello';
+    const numeros = [1,2,3,4,5,6,7,8,9];
+    const persona = {
+        nombre: "Gerardo",
+        apellido:"Lopez",
+        edad: 27
+    }
     return (
         <>
-            <h1>Hola Mundo</h1>
-            <p>Hola</p>
+            <h1>{saludo}</h1>
+            <h2>{numeros}</h2>
+            <h3>{JSON.stringify(persona, null,3)}</h3>
+            <pre>{JSON.stringify(persona, null,3)}</pre>
+           <p>Hola</p>
+
         </>
         );
 }
