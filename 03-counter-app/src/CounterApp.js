e21dd3fb279efd2217f5392f6d11a7e84e1e365c
GerardoLopez94 <gerardo.a.lopez1994@gmail.com>
2022-01-10 15:21:00 -0300

    Agregamos btn de reset y menos 1

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
 import  PropTypes  from "prop-types";
 
-const CounterApp = ({ value }) =>{
+const CounterApp = ({ value=10 }) =>{
 
-    const [counter, setCounter] = useState(0);
+    const [counter, setCounter] = useState(value);
 
     //handleApp
     const handleApp = () =>{
         setCounter( (counter + 1) );
+        //setCounter( (c) => c + 1)
+    }
+
+    const handleReset = () => ( setCounter( (counter) => (counter = value) )  );
+        
+    
+
+    const handleMenosUno = () =>{
+        setCounter( (counter - 1) );
         //setCounter( (c) => c + 1)
     }
     return (
@@ -15,6 +24,8 @@ const CounterApp = ({ value }) =>{
             <h1>CounterApp</h1>
             <h2>{ counter }</h2>
             <button onClick={handleApp}>+1</button>
+            <button onClick={handleReset}>reset</button>
+            <button onClick={handleMenosUno}>-1</button>
         </>
     )
 }
--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -6,4 +6,4 @@ import { CounterApp } from "./CounterApp";
 
 const divApp = document.querySelector('#app');
 
-ReactDOM.render(<CounterApp  />, divApp);
+ReactDOM.render(<CounterApp value = {0} />, divApp);
