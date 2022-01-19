e7666efacda9d889a16b5cf07952ac7dfa974029
GerardoLopez94 <gerardo.a.lopez1994@gmail.com>
2022-01-19 10:14:07 -0300

    Modificación en GifExpertApp - utilizamos el useState

--- 04-gif-expert-app/src/GifExpertApp.js
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState} from 'react';
 
 const GifExpertApp  = () =>{
 
-    const categories = ['One Punch', 'Samurai X','Dragom Ball'];
+    //const categories = ['One Punch', 'Samurai X','Dragom Ball'];
+    const [categories, setCategories] = useState(['One Punch', 'Samurai X','Dragom Ball'])
 
+    const handleApp = () => {
+        //Una forma
+        //setCategories( [ ...categories, 'Naruto' ] ); 
+        // otra forma
+        setCategories(  cats => [...cats, 'Naruto'] ); 
+    };
     return (
         <>
             <h2>GifExpertApp</h2>
             <hr />
+            <button onClick={handleApp}>Agregar</button>
             <ol>
                 {
                     categories.map( category=> {
