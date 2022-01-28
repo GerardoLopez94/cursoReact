2c1758e949479d4da3fa0454f8bdebc41ca34f00
GerardoLopez94 <gerardo.a.lopez1994@gmail.com>
2022-01-28 07:06:39 -0300

    pruebas de GitGridItem

--- 04-gif-expert-app/src/tests/components/GifGridItem.test.js
+++ 04-gif-expert-app/src/tests/components/GifGridItem.test.js
@@ -6,15 +6,42 @@ import '@testing-library/jest-dom'
 import { GifGridItem } from '../../components/GifGridItem';
 
 describe('Pruebas en <GifGridItem />', () => {
-    let wrapper = shallow(<GifGridItem img={[]} />);
-
-    beforeEach( () =>{
-        wrapper = shallow(<GifGridItem img={[]} />);
-    })
-
+    
+    const title = 'google';
+    const url = 'http://google.com';
+    let wrapper = shallow(<GifGridItem title={title} url={url} />);
+    
+    beforeEach(() =>{
+        wrapper = shallow(<GifGridItem title={title} url={url} />);
+    });
+    
     test('debe de mostrar el componente correctamente', () =>{
-
         expect(wrapper).toMatchSnapshot();
+    });
+
+    test('debe de tener un párrafo con un texto', () =>{
+        const textValor = wrapper.find('p').text().trim();
+
+        expect(typeof textValor).toBe('string');
+    });
+
+    test('debe de tener una imagen igual al url y alt de los props', () =>{
+        const img = wrapper.find('img');
+
+        //console.log(img.props()); //objeto son sus propiedades
+        
+        expect(img.props().src).toBe(url);
+        expect(img.props().alt).toBe(title);
+    });
+
+    test('debe de tener animate__bounceIn', () =>{
+        //referencia al div
+        const div = wrapper.find('div');
+
+        const className = div.props().className;
+
+        expect(className.includes('animate__bounceIn')).toBe(true);
+        
     })
 });
 
